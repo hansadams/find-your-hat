4a5cffc5269fc3797298e2a1cb66a78c5786f065
Hans Adams <hansadams@Hanss-MacBook-Air.local>
2023-10-30 08:50:17 +0800

    final

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const pathCharacter = '*';
 class Field {
   constructor(field) {
     this._field = field;
+    this._field[0][0] = pathCharacter
   }
 
   print() {
@@ -22,13 +23,32 @@ class Field {
     console.log (element.join('')))
   }
 
+  static generateField(height, width, percentage = 0.1) {
+    const field = new Array(height).fill(0).map(el => new Array(width));
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const prob = Math.random();
+        field[y][x] = prob > percentage ? fieldCharacter : hole;
+      }
+    }
+    // Set the "hat" location
+    const hatLocation = {
+      x: Math.floor(Math.random() * width),
+      y: Math.floor(Math.random() * height)
+    };
+    // Make sure the "hat" is not at the starting point
+    while (hatLocation.x === 0 && hatLocation.y === 0) {
+      hatLocation.x = Math.floor(Math.random() * width);
+      hatLocation.y = Math.floor(Math.random() * height);
+    }
+    field[hatLocation.y][hatLocation.x] = hat;
+    return field;
+  }
+
 }
 
-const myField = new Field([
-  ['*', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░']
-]);
+const myField = new Field(Field.generateField(10, 10, 0.2));
+console.log(myField)
 
 // intro before game start
 console.log ('WELCOME TO FIND YOUR HAT')
